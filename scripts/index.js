38a361e64da942eefed25e063c3906d9c1c67fb1
Maddy <maduvce26@gmail.com>
2020-05-15 19:46:33 +0530

    seperation of css and js

---
+++ scripts/index.js
@@ -0,0 +1,7 @@
+function sidemenuopen() {
+    document.getElementById('nav').classList.add('open');
+}
+
+function sidemenuclose() {
+    document.getElementById('nav').classList.remove('open');
+}
